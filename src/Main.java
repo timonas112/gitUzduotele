cab8890830827482b2ad381aec5ffd3b4ccb9e8c
Tim <timonasangelis@gmail.com>
2022-10-06 20:01:26 +0300

    uzduotis1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        Float rez;
+        a++;
+        b--;
+        rez =  a * b * (float) 5;
+        System.out.println(rez);
+
+    }
+}
