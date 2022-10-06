acaffa47c259bbbef030b4b919467262c02eb595
Tim <timonasangelis@gmail.com>
2022-10-06 20:19:00 +0300

    uzduotis1 uzduotis2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int a = 0;
         int b = 0;
         Float rez;
         a++;
         b--;
-        rez =  a * b * (float) 5;
+        rez = a * b * (float) 5;
         System.out.println(rez);
+
+
+        String[] dienos = {"Pirmadienis", "Antradienis", "Treciadienis", "Ketvirtadienis", "Penktadienis", "Sestadienis", "Sekmadienis" };
+        System.out.println("Masyvo dydis - " + dienos.length);
+        System.out.println("Paskutinis masyvo elementas - " + dienos[dienos.length - 1]);
+        System.out.println("Iveskite skaiciu nuo 1 iki 7.");
+        int input = sc.nextInt();
+        if (input > 7) {
+            System.out.println("Skaicius per didelis");
+        } else {
+            System.out.println(" Pasirinkta diena - " + dienos[input - 1]);
+        }
 
     }
 }
